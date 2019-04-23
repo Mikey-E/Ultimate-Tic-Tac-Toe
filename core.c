b355e11ccfbf8a5ae30818b0001e1e54a835690f
Michael Elgin <michaelelgin1@gmail.com>
2019-04-23 15:47:30 -0400

    cmd line flags for agents 1 and 2, history file as well cmd line flag for custom history file

--- core.c
+++ core.c
@@ -191,38 +191,59 @@ int sumEmptySpaces(char** board){
 }
 
 //sets the move function for an agent
-void setMoveFunction(void (**fPtrPtr)(char**, int*, int, int, int, int, char), char myChar){
-	char agentType;
-	do{
-		agentType = fgetc(stdin);
-		agentType = agentType - 48;//compensates for ascii
-		fflush(stdin);
-	}while ((agentType < 1) || (agentType > 3));//update if more agents are added in the future
-		printf("Setting agent type as %d.\n", agentType);
-	if (agentType == 1){
-		*fPtrPtr = &playHumanMove;
-	}else if (agentType == 2){
-		*fPtrPtr = &playRandomMove;
-	}else if (agentType == 3){
-		*fPtrPtr = &playMCTSMove;
-		char searchDepth;
-		printf("What search depth should this MCTS agent have (2 to 9)?\n");
-		printf("2 = easiest (minimum)\n");
-		printf("3 = easy\n");
-		printf("4 = medium\n");
-		printf("5 = challenging\n");
-		printf("6+ = very difficult (and very slow to make moves)\n");
-		printf("Enter search Depth: ");
+//moveType == 0 means no move type yet, get user input.
+//depth is for MCTS agents (3) only.
+void setMoveFunction(void (**fPtrPtr)(char**, int*, int, int, int, int, char), char agentChar, int moveType, int depth){
+	if (moveType){//already given move type
+		agentTypeHashTable[agentChar] = moveType;
+		if (moveType == 1){
+			*fPtrPtr = &playHumanMove;
+		}else if (moveType == 2){
+			*fPtrPtr = &playRandomMove;
+		}else if (moveType == 3){
+			*fPtrPtr = &playMCTSMove;
+			if (depth < 2){//should never happen, but just in case
+				depth = 2;
+			}
+			if (depth > 9){//should never happen, but just in case
+				depth = 9;
+			}
+			depthHashTable[agentChar] = (int) depth;//each MCTS agent's move function will retrieve its depth
+		}
+	}else{//get user input
+		char agentType;
 		do{
-			searchDepth = fgetc(stdin);
-			searchDepth = searchDepth - 48;//compensates for ascii
+			agentType = fgetc(stdin);
+			agentType = agentType - 48;//compensates for ascii
 			fflush(stdin);
-		}while ((searchDepth < 2) || (searchDepth > 9));
-		//ALWAYS keep depth as at least 2.
-		//0 wouldn't make any sense, and also probably cause seg fault
-		//1 leaves tree with nothing to compare with after opponent's move, also seg fault.
-		printf("Setting search depth as %d.\n", searchDepth);
-		depthHashTable[myChar] = (int) searchDepth;//each MCTS agent's move function will retrieve this depth
+		}while ((agentType < 1) || (agentType > 3));//update if more agents are added in the future
+			printf("Setting agent type as %d.\n", agentType);
+			agentTypeHashTable[agentChar] = (int) agentType;
+		if (agentType == 1){
+			*fPtrPtr = &playHumanMove;
+		}else if (agentType == 2){
+			*fPtrPtr = &playRandomMove;
+		}else if (agentType == 3){
+			*fPtrPtr = &playMCTSMove;
+			char searchDepth;
+			printf("What search depth should this MCTS agent have (2 to 9)?\n");
+			printf("2 = easiest (minimum)\n");
+			printf("3 = easy\n");
+			printf("4 = medium\n");
+			printf("5 = challenging\n");
+			printf("6+ = very difficult (and very slow to make moves)\n");
+			printf("Enter search Depth: ");
+			do{
+				searchDepth = fgetc(stdin);
+				searchDepth = searchDepth - 48;//compensates for ascii
+				fflush(stdin);
+			}while ((searchDepth < 2) || (searchDepth > 9));
+			//ALWAYS keep depth as at least 2.
+			//0 wouldn't make any sense, and also probably cause seg fault
+			//1 leaves tree with nothing to compare with after opponent's move, also seg fault.
+			printf("Setting search depth as %d.\n", searchDepth);
+			depthHashTable[agentChar] = (int) searchDepth;//each MCTS agent's move function will retrieve this depth
+		}
 	}
 }
 
@@ -269,11 +290,28 @@ void updateSmallSectorBoard(char** smallSectorBoard, int sector, char updateChar
 int main(int argc, char** argv){
 	//variables to be possibly changed by command line flags, with default values
 	int sleepSeconds = 1;
-	int sRand = 0;//can set a specific random number seed [1, 2^32 - 1]. 0 means base on current time
+	time_t rngSeed = 0;//can set a specific random number seed [1, 2^32 - 1]. 0 means base on current time
+	time_t t;//used for file timestamps
+	char* outputFileStr = malloc(12 * sizeof(char));
+	strcpy(outputFileStr, "history.txt");//where to append results of a game played
+	void (*agentFunction1)(char**, int*, int, int, int, int, char) = NULL;//called for moves
+	void (*agentFunction2)(char**, int*, int, int, int, int, char) = NULL;//"...
+	
 	int option;
-	while ((option = getopt(argc, argv, "hr:s:")) != -1){
+	while ((option = getopt(argc, argv, "1:2:f:hr:s:")) != -1){
 		switch(option){
-			case 'h':
+			case '1'://first agent type
+				setMoveFunction(&agentFunction1, PLAYER1CHAR, optarg[0] - 48, optarg[1] - 48);//-48 for ascii
+				break;
+			case '2'://second agent type
+				setMoveFunction(&agentFunction2, PLAYER2CHAR, optarg[0] - 48, optarg[1] - 48);//"...
+				break;
+			case 'f'://custom results file
+				free(outputFileStr);
+				outputFileStr = malloc(strlen(optarg) + sizeof(char));//+1 char for null terminator
+				strcpy(outputFileStr, optarg);
+				break;
+			case 'h'://help
 				printf("Rules:\n");
 				printf("Conquer 3 sectors just like in normal tic-tac-toe to achieve victory.\n");
 				printf("Each sector is its own tic-tac-toe game.\n");
@@ -281,24 +319,24 @@ int main(int argc, char** argv){
 				printf("opponent has to play in.\n");
 				printf("If that sector is full or conquered, then your opponent may play anywhere.\n");
 				printf("Enter row and col as numbers together. Ex: row 7, col 1 is \"71\", then enter.\n");
-				exit(0);
-			case 'r':
-				sRand = atoi(optarg);
+				return(0);
+			case 'r'://specific random number seed
+				rngSeed = atoi(optarg);
 				break;
-			case 's':
+			case 's'://seconds of sleep between each round
 				sleepSeconds = atoi(optarg);
 				break;
 			default:
 				printf("Usage: uttt [-h]\n-h:\tHelp. (Display rules.)\n");
-				exit(0);
+				return(0);
 		}
 	}
-	if (sRand){//set that specific seed
-		srand(sRand);
-	}else{//use current time
-		time_t t;
-		srand(time(&t));
+	if (!rngSeed){//use current time
+		rngSeed = time(&t);
 	}
+	srand(rngSeed);
+
+	//other necessary local variables
 	int i;
 	char** board = initBoard(9);
 	char** smallSectorBoard = initBoard(3);//for keeping track of won sectors, and therefore computing winner
@@ -310,20 +348,48 @@ int main(int argc, char** argv){
 	int isFullSector;
 	int changedSector;
 	char currChar;
-	char potentialWinner;//for checking if overall game has been won each round
-	void (*agentFunction1)(char**, int*, int, int, int, int, char);//called for moves
-	void (*agentFunction2)(char**, int*, int, int, int, int, char);//"
+	char potentialWinner = '\0';//for checking if overall game has been won each round
 
 	printf("\nUltimate Tic-Tac-Toe\n");//initial message
 
 	//Select what agent to play against
-	printf("1 = Human\n");
-	printf("2 = Random Move Maker (very easy)\n");
-	printf("3 = Monte Carlo Tree Search (challenging)\n");
-	printf("What type of player is player 1: ");
-	setMoveFunction(&agentFunction1, PLAYER1CHAR);
-	printf("What type of player is player 2: ");
-	setMoveFunction(&agentFunction2, PLAYER2CHAR);
+	if ((agentFunction1 == NULL) || (agentFunction2 == NULL)){
+		printf("1 = Human\n");
+		printf("2 = Random Move Maker (very easy)\n");
+		printf("3 = Monte Carlo Tree Search\n");
+	}
+	if (agentFunction1 == NULL){
+		printf("What type of player is player 1: ");
+		setMoveFunction(&agentFunction1, PLAYER1CHAR, 0, 0);//no agent type given yet, n/a search depth
+	}
+	if (agentFunction2 == NULL){
+		printf("What type of player is player 2: ");
+		setMoveFunction(&agentFunction2, PLAYER2CHAR, 0, 0);//"...
+	}
+
+	//append some info for output file
+	//appending some information early (such as rng seed) can help with debugging in gdb if there is a game crash.
+	FILE* resultsFilePtr = fopen(outputFileStr, "a");//append mode
+	fprintf(resultsFilePtr, "---Game---\n");
+	time(&t);//get time game started
+	char* startTimestamp = ctime(&t);	
+	fprintf(resultsFilePtr, "Started: %s", startTimestamp);
+	fprintf(resultsFilePtr, "Random Number Seed: %d\n", rngSeed);
+	fprintf(resultsFilePtr,
+			"Player 1: %c, Type: %d",
+			PLAYER1CHAR, agentTypeHashTable[PLAYER1CHAR]);
+	if (agentTypeHashTable[PLAYER1CHAR] == 3){
+		fprintf(resultsFilePtr, ", Search Depth: %d", PLAYER1CHAR, depthHashTable[PLAYER1CHAR]);
+	}
+	fprintf(resultsFilePtr,
+			"\nPlayer 2: %c, Type: %d",
+			PLAYER2CHAR, agentTypeHashTable[PLAYER2CHAR]);
+	if (agentTypeHashTable[PLAYER2CHAR] == 3){
+		fprintf(resultsFilePtr, ", Search Depth: %d", PLAYER2CHAR, depthHashTable[PLAYER2CHAR]);
+	}
+	fprintf(resultsFilePtr, "\n");
+	fclose(resultsFilePtr);//write
+	resultsFilePtr = fopen(outputFileStr, "a");//reopen
 
 	printBoard(board, -1, -1);//initial empty board
 
@@ -353,12 +419,14 @@ int main(int argc, char** argv){
 				if (potentialWinner != '\0'){//we have a winner
 					printBoard(board, -1, -1);
 					printf("%c Wins!\n", potentialWinner);
+					fprintf(resultsFilePtr, "Winner: %c\n", potentialWinner);
 					goto cleanUpMemory;
 				}
 			}
 			if (sumEmptySpaces(board) == 0){//draw
 				printBoard(board, -1, -1);
 				printf("Tie!\n");
+				fprintf(resultsFilePtr, "Winner: tie");
 				goto cleanUpMemory;
 			}
 			updateBounds(rowMove, colMove, board, &minRow, &maxRow, &minCol, &maxCol, &isFullSector);
@@ -388,5 +456,14 @@ cleanUpMemory:
 		freeTree(treeHashTable[PLAYER2CHAR]);
 	}
 	free(moves);
+
+	//append results to file
+	time(&t);//get time game finished
+	char* endTimestamp = ctime(&t);	
+	fprintf(resultsFilePtr, "Ended: %s", endTimestamp);
+	fprintf(resultsFilePtr, "----------\n");
+
+	fclose(resultsFilePtr);
+	free(outputFileStr);
 	return 0;//Success.
 }
--- core.h
+++ core.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 #include <windows.h>//not needed for compiling on linux
 
 //function declarations
@@ -17,9 +18,11 @@ void convertSectorToIndexes(int sector, int* rowIndex, int* colIndex);
 char examineSectorForWinner(char** board, int sector);
 int updateBoardStatus(char** board);
 int sumEmptySpaces(char** board);
-void setMoveFunction(void (**fPtrPtr)(char**, int*, int, int, int, int, char), char myChar);
+void setMoveFunction(void (**fPtrPtr)(char**, int*, int, int, int, int, char), char agentChar, int moveType, int depth);
 void updateBounds(int rowMove, int colMove, char** board, int* minRow, int* maxRow, int* minCol, int* maxCol, int* isFullSector);
 void updateSmallSectorBoard(char** smallSectorBoard, int sector, char updateChar);
+
+int agentTypeHashTable[256];//for storing information about what type each agent is
 
 // -- SETTINGS --
 #define PLAYER1CHAR 'X'
